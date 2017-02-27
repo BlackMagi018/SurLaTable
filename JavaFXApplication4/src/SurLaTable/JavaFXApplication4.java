5062d1b7812d5fde50d05f6c744b850e83d04609
BlackMagi018 <Hawkman063@aol.com>
2017-02-27 09:34:50 -0500

    Add files via upload

---
+++ JavaFXApplication4/src/SurLaTable/JavaFXApplication4.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SurLaTable;
+
+import javafx.application.Application;
+import javafx.event.EventType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Jeremiah
+ */
+public class JavaFXApplication4 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("SearchStart.fxml"));
+        
+        Scene Splash = new Scene(root);
+        
+        stage.setTitle("Sur La Table");
+        stage.setScene(Splash);
+        stage.show();
+        
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
